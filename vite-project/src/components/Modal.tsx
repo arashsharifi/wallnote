e9002541bbe6ea62154b661df310b49cf6695b17
arash amirsharifi <arashamirsharifi1970@gmail.com>
2024-08-31 23:35:03 +0330

    add : component addform v2 fix:Navcart fix: style model add:doc for README.MD

--- vite-project/src/components/Modal.tsx
+++ vite-project/src/components/Modal.tsx
@@ -15,7 +15,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children,applicationStat
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div
         className={`bg-white rounded-lg shadow-lg p-4 w-full max-w-md mx-auto relative ${
-          applicationStatus === "deletcomponent" ? "h-[300px]" : "h-[600px]"
+          applicationStatus === "deletcomponent" ? "h-[300px]" : "h-[640px]"
         }`}
       >
         <button
